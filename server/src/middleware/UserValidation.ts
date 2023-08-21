3c6604dd247f6b94293032518ae678d6123b92fa
Akbar Ali <akbar1991ali@yahoo.com>
2023-08-21 22:15:58 +0530

    API input validation

--- server/src/middleware/UserValidation.ts
+++ server/src/middleware/UserValidation.ts
@@ -2,8 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
 
 const userSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
+  firstName: Joi.string().max(100).required().regex(/^[A-Za-z ]{1,100}$/).messages(
+    {
+      'any.required': 'First Name is Required',
+      'any.max': 'Accepts max 100 character',
+      'string.pattern.base': 'First name should contain only alphabetical characters and spaces',
+    }),
+  lastName:Joi.string().max(100).required().regex(/^[A-Za-z ]{1,100}$/).messages(
+    {
+      'any.required': 'Last Name is Required',
+      'any.max': 'Accepts max 100 character',
+      'string.pattern.base': 'last name should contain only alphabetical characters and spaces',
+    }),
   email: Joi.string().email().required(),
 }).options({
   abortEarly: false,
